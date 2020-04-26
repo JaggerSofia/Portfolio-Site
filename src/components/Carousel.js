1c73292d196016482547b1c8e00834ba017e4888
Victoria Moore <victoriamoore@Victorias-MacBook-Pro.local>
2020-04-26 12:31:16 -0700

    created favicon

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -23,14 +23,14 @@ class Carousel extends React.Component {
                     link: 'https://get-meals-app.now.sh/',
                     selected: false
                 },
-                {
-                    id: 1,
-                    title: 'BookNation',
-                    subTitle: 'An all in one stop to review books',
-                    imgSrc: BookNation,
-                    link: 'https://booknation-app.now.sh/',
-                    selected: false
-                },
+                // {
+                //     id: 1,
+                //     title: 'BookNation',
+                //     subTitle: 'An all in one stop to review books',
+                //     imgSrc: BookNation,
+                //     link: 'https://booknation-app.now.sh/',
+                //     selected: false
+                // },
                 {
                     id: 2,
                     title: 'Doctor Who Quiz App',
